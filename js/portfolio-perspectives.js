8e68f165482c60d4e6ceeb94c69fdca7f92b252f
Stephen Crockett <scrockett@planview.com>
2014-03-08 09:56:04 -0600

    Add Munchkin code

--- js/portfolio-perspectives.js
+++ js/portfolio-perspectives.js
@@ -4,3 +4,11 @@
 
 
 jQuery.webshims.polyfill();
+jQuery.ajax({
+    url: '//munchkin.marketo.net/munchkin.js',
+    dataType: 'script',
+    cache: true,
+    success: function() {
+        Munchkin.init('587-QLI-337');
+    }
+});
